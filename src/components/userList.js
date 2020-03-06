169187cf064e3ab013352a0bedcc3e983381f327
Marc Majcher <majcher@gmail.com>
2020-03-06 16:56:03 -0600

    eod

--- src/components/userList.js
+++ src/components/userList.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const BASE_URL = 'http://localhost:8000';
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
+  const userInfo = useSelector(store => store.userInfo);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/users/`).then(response => setUsers(response.data));
-  }, []);
+    axios
+      .get(`${BASE_URL}/users/`)
+      .then(response =>
+        setUsers(
+          response.data.filter(user => user.googleId !== userInfo.googleId)
+        )
+      );
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  return (
+  return users.length > 0 ? (
     <ul>
       {users.map(user => (
-        <li key={user.googleId}><a href="/" alt="chat">[chat]</a> {user.name} &lt;{user.email}&gt;</li>
+        <li key={user.googleId}>
+          <a href="/" alt="chat">
+            [chat]
+          </a>{' '}
+          {user.name} &lt;{user.email}&gt;
+        </li>
       ))}
     </ul>
+  ) : (
+    <div>No other users!</div>
   );
 }
