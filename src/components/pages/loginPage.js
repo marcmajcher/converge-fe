0f68e3dbefb85fd3ae30662fbe47ab8d0899408e
Marc Majcher <majcher@gmail.com>
2020-04-03 12:57:13 -0500

    wip

--- src/components/pages/loginPage.js
+++ src/components/pages/loginPage.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 
 export default function LoginPage() {
-  const loggedIn = useSelector(s => s.loggedIn);
-
-  return loggedIn ? (
-    <Redirect to="/" />
-  ) : (
+  return (
     <div>
       <h1>LOG IN -----> over there</h1>
     </div>
