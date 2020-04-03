975453e498c2633baf72728624478b878ebce6b6
Marc Majcher <majcher@gmail.com>
2020-04-02 21:53:56 -0500

    alpha

---
+++ build/precache-manifest.5163b42e8c2f6b20bbb7b3a41ed9179c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1e2dff26041115166e5293147788c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec20d0c0c7c3cd1c3013",
+    "url": "/static/css/main.a0c2b061.chunk.css"
+  },
+  {
+    "revision": "ab26675338f43073f0eb",
+    "url": "/static/js/2.ded1aff5.chunk.js"
+  },
+  {
+    "revision": "bc05de9fbe120b80f2ed394de83935eb",
+    "url": "/static/js/2.ded1aff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec20d0c0c7c3cd1c3013",
+    "url": "/static/js/main.f33ec51f.chunk.js"
+  },
+  {
+    "revision": "77e74c1bb988f813c96a",
+    "url": "/static/js/runtime-main.8078038c.js"
+  }
+]);
